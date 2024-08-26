5590e80ba21bd7a69facceaca460090bf552eb40
bruno-globals1 <bruno.guerra@globals.one>
2024-08-26 11:02:07 -0500

    feat: 🎸 add service section and components
    add service section

---
+++ src/app/services/page.tsx
@@ -0,0 +1,13 @@
+import OurTeam from "../../components/common/OurTeam";
+import Testimonials from "../../components/common/Testimonials";
+import Quote from "./components/Quote";
+
+export default function Service() {
+  return (
+    <div>
+      <Quote/>
+      <OurTeam />
+      <Testimonials />
+    </div>
+  );
+}
