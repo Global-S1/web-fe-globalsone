0e76fe2121fdbe1b0918c53f6cf35fe7b69c5ae6
JulianGS1 <julian.agama@globals.one>
2024-10-08 20:41:25 -0500

    fix: 🐛 fix font family in all home
    ✅ Closes: IGS-1

--- src/shared/components/footer/footer-col-index/index.tsx
+++ src/shared/components/footer/footer-col-index/index.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import s from "./footer-col-index.module.css";
 
-export const ColIndex = ({ title }) => {
+export const ColIndex = ({ title }: { title: string }) => {
   return (
     <div className={s.col_index_container}>
       <h3>{title}</h3>
