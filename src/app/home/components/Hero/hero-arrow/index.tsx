9788d607e64d5e8406f5da2997d0163c93be15b1
JulianGS1 <julian.agama@globals.one>
2024-10-10 11:41:55 -0500

    fix: 🐛 fix service button
    ✅ Closes: IGS-1

--- src/app/home/components/Hero/hero-arrow/index.tsx
+++ src/app/home/components/Hero/hero-arrow/index.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 import { DobleDonwArrow } from "@/assets/home/hero/icons/DobleDonwArrow";
 import s from "./hero-arrow.module.css";
 
 export const HeroArrow = () => {
+  const handleMetrics = () => {
+    document.getElementById("metrics")?.scrollIntoView({ behavior: "smooth" });
+  };
   return (
-    <a href="#metrics" className={s.hero__arrow}>
+    <button onClick={handleMetrics} className={s.hero__arrow}>
       <div className={s.arrow__container}>
         <DobleDonwArrow />
       </div>
@@ -11,6 +16,6 @@ export const HeroArrow = () => {
       <div className={s.arrow__container}>
         <DobleDonwArrow />
       </div>
-    </a>
+    </button>
   );
 };
